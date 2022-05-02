f5e6cf9858a071971843f251938e1e0a50e3bce6
pierfrancescoscar <scarinci401@gmail.com>
2022-05-02 16:40:52 +0200

    complete functionality, delete single record, reload, delete all

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import List from './components/List';
+import data from './data';
+import {useState} from "react";
+
 function App() {
+const [people, setPeople] = useState(data);
+
+const removeItem = (id) => {
+  setPeople((oldValue) => oldValue.filter((value) => value.id !== id));
+};
+
+const reloadAllItem = () => {
+  setPeople(data);
+};
+
   return (
     <div className="App">
-      <h2 className="d-flex justify-content-center my-3">Appointments App</h2>
+      <div className='container rounded shadow mt-5 p-3 bg-white'>
+        {/* Title */}
+        <h3 className="d-flex justify-content-center my-3">Appointments App</h3>
+
+        {/* People List */}
+        <div>
+
+          <List data={people} removeItem={removeItem} />
+
+        </div>
+
+        {/* Buttons */}
+        <div className='btn-group'>
+
+          <button className="btn btn-reset" onClick={reloadAllItem}>
+              {" "}
+              Reload{" "}
+          </button>
+
+          <button className="btn btn-delete" onClick={() => setPeople([])}>
+            {" "}
+            Delete all
+          </button>
+
+        </div>
+
+      </div>
     </div>
   );
 }
